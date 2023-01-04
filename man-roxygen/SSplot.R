aa21e76cd7795996fb0b76470c700b68710e012b
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2023-01-03 15:35:19 -1000

    Added inital SSplot roxygn template

---
+++ man-roxygen/SSplot.R
@@ -0,0 +1,32 @@
+#' @param pwidth
+#' Width of the plot
+#' 
+#' @param pheight
+#' Height of the plot
+#' 
+#' @param punits
+#' Measurement units for output PNG image file 
+#' 
+#' @param res
+#' Resolution for output PNG image file
+#' 
+#' @param ptsize
+#' Point size for output PNG image file
+#' 
+#' @param cex.main
+#' Character Expansion for plot titles. See \code{\link{par}} for more info.
+#' 
+#' @param filenameprefix
+#' Additional text to append to output image file name. It will be separated from the 
+#' default name by an underscore.
+#' 
+#' @param plotdir
+#' Directory where output image files will be written. By default,
+#' it will be the directory where the model was run.
+#' 
+#' @param par
+#' list of graphics parameter values passed to \code{\link{par()}} function
+#' 
+#' @param verbose
+#' Option to report progress to R console. Default is TRUE.
+#' 
