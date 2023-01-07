8b35b136ec950bf743a5a645791233981a6e2929
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2023-01-06 16:27:28 -1000

    Added legend roxyen template

---
+++ man-roxygen/legend.R
@@ -0,0 +1,29 @@
+#' @param legend
+#' Option to add a legend. TRUE by default.
+#' 
+#' @param legendlabels
+#' Optional vector of labels to include in legend.
+#' 
+#' @param legendloc
+#' Location of legend. Either a string like `topleft` or a vector of two 
+#' numeric values representing the fraction of the maximum in the x and y
+#' dimensions, respectively. See [`?legend`][graphics::legend()] for more info on the 
+#' string options. 
+#' 
+#' @param legendorder
+#' Optional vector of model numbers that can be used to have the legend 
+#' display the model names in an order that is different than that
+#' which is represented in the summary input object.
+#' 
+#' @param legendncol
+#' Number of columns for the legend
+#' 
+#' @param legendcex
+#' Allows to adjust [`legend`][graphics::legend()] cex
+#' 
+#' @param legendsp
+#' Space between legend labels
+#' 
+#' @param legendindex
+#' Allows to add legend for selected indices (plots)
+#' 
