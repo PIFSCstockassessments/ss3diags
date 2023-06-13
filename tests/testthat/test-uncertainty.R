7a5184c3e0fbb0820ebb26b64a99088b96e3996d
MOshima-PIFSC <megumi.oshima@noaa.gov>
2023-06-12 14:59:46 -1000

    update test simple object to use r4ss simple_small

--- tests/testthat/test-uncertainty.R
+++ tests/testthat/test-uncertainty.R
@@ -1,18 +1,18 @@
 # Test script for evaluating model uncertainty
+simple <- r4ss::SS_output(dir = test_example_path, verbose=FALSE, printstats = FALSE)
 
-simple <- ss3diags::simple
 
 mvln.msy <- SSdeltaMVLN(simple, Fref = "MSY")
 
 test_that("mle table values are as expected for Simple with Fref = MSY", {
-  expect_equal(round(mvln.msy$mle$harvest[1], 7), 0.8845945)
-  expect_equal(round(mvln.msy$mle$stock[1], 7), 2.0921525)
-  expect_equal(round(mvln.msy$mle$F[1], 7), 0.1009570)
+  expect_equal(round(mvln.msy$mle$harvest[1], 7), 1.210293)
+  expect_equal(round(mvln.msy$mle$stock[1], 7), 1.0998)
+  expect_equal(round(mvln.msy$mle$F[1], 7), 0.239068)
 })
 
 
 test_that("Kobe plot values are correct", {
-  kb <- mvln.msy$kb[which(mvln.msy$kb$year == 100), ] # last year of model
+  kb <- mvln.msy$kb[which(mvln.msy$kb$year == 2022), ] # last year of model
 
   kb$green <- ifelse(kb$stock > 1 & kb$harvest < 1, 1, 0)
   kb$red <- ifelse(kb$stock < 1 & kb$harvest > 1, 1, 0)
