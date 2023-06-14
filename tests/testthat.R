27a11c39da93535583aef278d9efb334bc3edd2e
MOshima-PIFSC <megumi.oshima@noaa.gov>
2023-06-13 16:19:22 -1000

    add cleanup

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,11 @@
 library(testthat)
 library(ss3diags)
+library(r4ss)
 
 test_example_path <- system.file("extdata", "simple_small", package = "r4ss")
 
 # Creating retrospective object here so that multiple test files can access it without having to re-run retrospective for each test
-retroModels <- SSgetoutput(
+retroModels <- r4ss::SSgetoutput(
     dirvec = file.path(
       test_example_path, "retrospectives",
       paste0("retro", 0:-3)
