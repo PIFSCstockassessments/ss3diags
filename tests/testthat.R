7a709afaedf7b48d4925ac4e3b0e707f7c23f61d
MOshima-PIFSC <megumi.oshima@noaa.gov>
2023-06-14 07:23:01 -1000

    test-fixtures path

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,7 @@ library(ss3diags)
 library(r4ss)
 
 test_example_path <- system.file("extdata", "simple_small", package = "r4ss")
-test_fixtures <- file.path(getwd(), "tests", "testthat", "fixtures")
+test_fixtures <- system.file("tests", "testthat", "fixtures" package = "ss3diags")
 #testthat::test_path("fixtures")
 
 # Creating retrospective object here so that multiple test files can access it without having to re-run retrospective for each test
