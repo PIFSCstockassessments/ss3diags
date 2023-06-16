09a31dfe32994bd55d423f92a2f9fb6399731bfd
MOshima-PIFSC <megumi.oshima@noaa.gov>
2023-06-16 08:48:47 -1000

    run retrospectives in test

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,7 +5,8 @@ library(r4ss)
 test_example_path <- system.file("extdata", "simple_small", package = "r4ss")
 
 ## Run retrospectives
-#r4ss::retro(dir = runs_path, oldsubdir = "", newsubdir = "retrospectives", years = retro_years, show_in_console = FALSE)
+runs_path <- file.path(".", "model_recipes", "test")
+r4ss::retro(dir = runs_path, oldsubdir = "", newsubdir = "retrospectives", years = retro_years, show_in_console = FALSE)
          # unlink(file.path(runs_path, "retrospectives", "retro0", "ss"))
          # unlink(file.path(runs_path, "retrospectives", "retro-1", "ss"))
          # unlink(file.path(runs_path, "retrospectives", "retro-2", "ss"))
@@ -14,7 +15,7 @@ test_example_path <- system.file("extdata", "simple_small", package = "r4ss")
 
 # Creating retrospective object here so that multiple test files can access it without having to re-run retrospective for each test
 retroModels <- r4ss::SSgetoutput(
-    dirvec = file.path("model_recipes", "test", "retrospectives",
+    dirvec = file.path(runs_path, "retrospectives",
       paste0("retro", 0:-3)
     )
   )
