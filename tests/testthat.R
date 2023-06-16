b3d5803d057543c5f54e4c5a625f765d0a99f6f0
MOshima-PIFSC <megumi.oshima@noaa.gov>
2023-06-16 08:22:43 -1000

    put retro runs in data file

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,17 +4,8 @@ library(r4ss)
 
 test_example_path <- system.file("extdata", "simple_small", package = "r4ss")
 
-tmp_path <- file.path(tempdir(check = TRUE), "test-retro")
-dir.create(tmp_path, showWarnings = FALSE)
-runs_path <- file.path(tmp_path, "simple_small")
-file.copy(test_example_path, tmp_path, recursive = TRUE)
-file.copy(file.path("tests", "testthat", "ss"), runs_path)
-retro_years <- 0:-3
-# clean up
-on.exit(unlink(tmp_path, recursive = TRUE))
-
 ## Run retrospectives
-r4ss::retro(dir = runs_path, oldsubdir = "", newsubdir = "retrospectives", years = retro_years, show_in_console = FALSE)
+#r4ss::retro(dir = runs_path, oldsubdir = "", newsubdir = "retrospectives", years = retro_years, show_in_console = FALSE)
          # unlink(file.path(runs_path, "retrospectives", "retro0", "ss"))
          # unlink(file.path(runs_path, "retrospectives", "retro-1", "ss"))
          # unlink(file.path(runs_path, "retrospectives", "retro-2", "ss"))
@@ -24,7 +15,7 @@ r4ss::retro(dir = runs_path, oldsubdir = "", newsubdir = "retrospectives", years
 # Creating retrospective object here so that multiple test files can access it without having to re-run retrospective for each test
 retroModels <- r4ss::SSgetoutput(
     dirvec = file.path(
-      runs_path, "retrospectives",
+      file.path("data", "test", "retrospectives",
       paste0("retro", retro_years)
     )
   )
